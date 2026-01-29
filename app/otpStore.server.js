2f3881f03549613818170b72b2d16953dc5750ea
asadurrehm890 <asadurrehm890@gmail.com>
2026-01-29 16:43:13 +0500

    newest

--- app/otpStore.server.js
+++ app/otpStore.server.js
@@ -11,7 +11,7 @@ function getTransporter() {
     transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT || 587),
-      secure: true, // true for 465, false for 587
+      secure: false, // true for 465, false for 587
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
