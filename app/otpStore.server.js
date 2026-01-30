e95f5132ff7c1375f40e50b36661d9e7f0ed8ab1
asadurrehm890 <asadurrehm890@gmail.com>
2026-01-30 09:33:06 +0500

    neew

--- app/otpStore.server.js
+++ app/otpStore.server.js
@@ -9,9 +9,9 @@ let transporter;
 function getTransporter() {
   if (!transporter) {
     transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: Number(process.env.EMAIL_PORT || 587),
-      secure: true, // true for 465, false for 587
+      host: 'smtp-relay.brevo.com',
+      port: Number(587),
+      secure: false, // true for 465, false for 587
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
