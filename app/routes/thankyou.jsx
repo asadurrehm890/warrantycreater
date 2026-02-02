a852fd4dae2802b402c093539729af35de4ef891
asadurrehm890 <asadurrehm890@gmail.com>
2026-02-02 11:25:17 +0500

    newwest899

--- app/routes/thankyou.jsx
+++ app/routes/thankyou.jsx
@@ -1,11 +1,25 @@
 import "../styles/warranty.css"; 
 
 export default function ThankYouPage() {
+
+    function handlethanku() {
+        window.location.href = "/warranty";
+    }
     return (   
         <section className="thankyou-section">
             <div className="position-centered-thankyou">
             <h1>Thank You!</h1>
             <p>Your warranty registration has been successfully submitted.</p>
+             <div className="warranty-actions">
+            <button
+              className="warranty-button"
+              onClick={handlethanku}
+            >
+              Back
+            </button>
+
+            
+          </div>
             </div>
         </section>
     );
