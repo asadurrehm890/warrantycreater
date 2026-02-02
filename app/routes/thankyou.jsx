16981d8bfa3270ee0d313e3473b7c2bfaa79a245
asadurrehm890 <asadurrehm890@gmail.com>
2026-02-02 10:53:52 +0500

    newwest87474

---
+++ app/routes/thankyou.jsx
@@ -0,0 +1,8 @@
+export default function ThankYouPage() {
+    return (   
+        <section className="thankyou-section">
+            <h1>Thank You!</h1>
+            <p>Your warranty registration has been successfully submitted.</p>
+        </section>
+    );
+}
