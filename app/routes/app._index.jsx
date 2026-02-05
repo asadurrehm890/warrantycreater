1872b0b4b034fa7af7ef49552c0b9de2c440258f
asadurrehm890 <asadurrehm890@gmail.com>
2026-02-05 18:02:27 +0500

    justlastcommit done 0l343t9%

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,9 +1,9 @@
 export default function warranty() {
    return(
-    <s-page heading="Warranty form integration app">
-        <s-section heading="Vist https://mobitel.uk/pages/warranty-form">
-            <s-paragraph>Fill the form then generate warranty</s-paragraph>
-            <a href="https://admin.shopify.com/store/mobiteluk/content/metaobjects/entries/warranty_activation_details" target="_blank">View MetaObject Listing</a>
+    <s-page heading="Warranty Activation">
+        <s-section>
+            <s-paragraph>View, manage, and process all customer warranty registrations submitted through the warranty activation form <s-link href="https://mobitel.uk/pages/warranty-activation">https://mobitel.uk/pages/warranty-activation</s-link></s-paragraph>
+            <s-link href="https://admin.shopify.com/store/mobiteluk/content/metaobjects/entries/warranty_activation_details">View All Warranty Activation Requests</s-link>
         </s-section>
     </s-page>
    );
