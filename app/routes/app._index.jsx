5204cd3faf888479572b87d5acff58067fd03b11
asadurrehm890 <asadurrehm890@gmail.com>
2026-02-05 15:26:14 +0500

    justlastcommit done 8%

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,8 +1,9 @@
 export default function warranty() {
    return(
     <s-page heading="Warranty form integration app">
-        <s-section heading="Vist https://warrantycreater-production.up.railway.app/warranty">
+        <s-section heading="Vist https://mobitel.uk/pages/warranty-form">
             <s-paragraph>Fill the form then generate warranty</s-paragraph>
+            <s-link url="https://admin.shopify.com/store/mobiteluk/content/metaobjects/entries/warranty_activation_details">View MetaObject Listing</s-link>
         </s-section>
     </s-page>
    );
