5eef457b9e592cb31d5d84aeb2173418202325f2
asadurrehm890 <asadurrehm890@gmail.com>
2026-02-06 09:35:21 +0500

    commit 04

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -2,21 +2,28 @@ export default function Warranty() {
   return (
     <s-page heading="Warranty Activation">
       <s-section>
-        <div style={{ 
+        <div
+          style={{
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
             textAlign: 'center',
-            gap: '1rem', }}>
+            gap: '1rem',
+          }}
+        >
           <p>
             View, manage, and process all customer warranty registrations submitted through the warranty activation form{' '}
-            <s-link href="https://mobitel.uk/pages/warranty-activation">
+            <s-link
+              href="https://mobitel.uk/pages/warranty-activation"
+              target="_self"
+            >
               https://mobitel.uk/pages/warranty-activation
-            </s-link>.
+            </s-link>
           </p>
 
           <s-button
             href="https://admin.shopify.com/store/mobiteluk/content/metaobjects/entries/warranty_activation_details"
+            target="_self"
             variant="primary"
           >
             View All Warranty Activation Requests
